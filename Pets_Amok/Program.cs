9c3f42b459477228c425d1a81f0bda923f8f5a12
Brian <bpjd21@yahoo.com>
2019-01-17 13:42:25 -0500

    First Commit

---
+++ Pets_Amok/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pets_Amok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
